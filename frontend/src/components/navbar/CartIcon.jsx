226f1e2d1694ac9939bb6c5a7a0cd100bbb2ce2b
Sandro <vipsandro11@gmail.com>
2024-11-24 22:38:33 +0400

    update CartIcon.jsx

--- frontend/src/components/navbar/CartIcon.jsx
+++ frontend/src/components/navbar/CartIcon.jsx
@@ -3,7 +3,7 @@ import { FaShoppingCart } from "react-icons/fa";
 const CartIcon = () => {
   return (
     <li className="relative">
-      <FaShoppingCart className="text-[1.5rem]" />
+      <FaShoppingCart className="text-[1.5rem] dark:text-white" />
       <span
         className="absolute -right-1 -top-1 text-[.7rem] text-white w-[.9rem] h-[.9rem] bg-red-600
      rounded-full flex items-center justify-center"
