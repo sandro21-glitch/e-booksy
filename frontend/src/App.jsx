56ca5fb51d5074516d4b0cc51a120ffe0b60f7a7
Sandro <vipsandro11@gmail.com>
2024-11-24 22:38:23 +0400

    update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,39 @@
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Provider } from "react-redux";
+import { Provider, useSelector } from "react-redux";
 import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/home/Home";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
-import { store } from "./store";
 import ProfilePage from "./pages/userProfile/ProfilePage";
+import { useEffect } from "react";
 
 const App = () => {
+  const theme = useSelector((state) => state.theme.theme);
+  useEffect(() => {
+    const root = window.document.documentElement;
+    const body = window.document.body;
 
-  const jwtToken = localStorage.getItem("jwt");
-  console.log(jwtToken);
-  
+    if (theme === "dark") {
+      root.classList.add("dark");
+      body.classList.add("bg-gray-900");
+      body.classList.remove("bg-white");
+    } else {
+      root.classList.remove("dark");
+      body.classList.add("bg-white"); 
+      body.classList.remove("bg-gray-900"); 
+    }
+  }, [theme]);
+
   return (
-    <Provider store={store}>
-      <Router v7_startTransition>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </Router>
-    </Provider>
+    <Router v7_startTransition>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
   );
 };
 
