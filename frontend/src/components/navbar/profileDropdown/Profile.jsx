daadd5b6a54aa8f89861109b84a765b86f47e70a
Sandro <vipsandro11@gmail.com>
2024-11-24 22:38:41 +0400

    update Profile.jsx

--- frontend/src/components/navbar/profileDropdown/Profile.jsx
+++ frontend/src/components/navbar/profileDropdown/Profile.jsx
@@ -1,24 +1,25 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import LogoutBtn from "./LogoutBtn";
+import { Link } from "react-router-dom";
 
 const Profile = () => {
   const {
     userInfo: { name, email },
-  } = useSelector((store) => store.auth)
+  } = useSelector((store) => store.auth);
 
   return (
     <div className="dropdown dropdown-bottom dropdown-end">
       <div className="flex items-center gap-1 cursor-pointer" tabIndex="0">
-        <div className="rounded-full w-[3rem] h-[3rem] p-[2px] border-2 border-lightGray">
-          <div className="bg-lightGray w-full h-full rounded-full flex items-center justify-center">
-            <span className="text-black text-[.8rem]">{name.slice(0, 3)}</span>
+        <div className="rounded-full w-[3rem] h-[3rem] p-[2px] border-2 border-lightGray dark:border-gray-700">
+          <div className="bg-lightGray dark:bg-gray-700  w-full h-full rounded-full flex items-center justify-center">
+            <span className="text-black dark:text-white text-[.7rem]">{name.slice(0, 3)}</span>
           </div>
         </div>
-        <h6 className="text-[.9rem] capitalize font-semibold">{name}</h6>
+        <h6 className="text-[.9rem] dark:text-white capitalize font-semibold">{name}</h6>
       </div>
       <ul
-        tabindex="0"
+        tabIndex={0}
         className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow"
       >
         <li>
@@ -38,7 +39,9 @@ const Profile = () => {
         </li>
         <hr />
         <li>
-          <p>Profile</p>
+          <Link to={"/profile"} className="block">
+            Profile
+          </Link>
         </li>
         <LogoutBtn />
       </ul>
