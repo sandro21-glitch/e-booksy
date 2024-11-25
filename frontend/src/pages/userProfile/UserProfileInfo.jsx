5aab3122e68d439cfb716e93dba42bd5290bb056
Sandro <vipsandro11@gmail.com>
2024-11-25 16:05:40 +0400

    update UserProfileInfo.jsx

--- frontend/src/pages/userProfile/UserProfileInfo.jsx
+++ frontend/src/pages/userProfile/UserProfileInfo.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from "react";
 import { FaRegEdit } from "react-icons/fa";
-import { useGetUserQuery } from "../../api/apiUser";
+import { useGetUserQuery, useUpdateUserMutation } from "../../api/apiUser";
 import Loader from "../../components/Loader";
 import { useNavigate } from "react-router-dom";
+import UserRoleCard from "./UserRoleCard";
 
 const UserProfileInfo = () => {
   const { data: user, isLoading } = useGetUserQuery();
@@ -13,7 +14,7 @@ const UserProfileInfo = () => {
     if (!isLoading && !user) {
       navigate("/login");
     }
-  }, [user, isLoading, navigate]); 
+  }, [user, isLoading, navigate]);
 
   if (isLoading) {
     return (
@@ -32,23 +33,7 @@ const UserProfileInfo = () => {
           {name.slice(0, 3)}
         </p>
       </div>
-      <div className="flex flex-col gap-[1px]">
-        <p className="text-[1rem] font-bold capitalize dark:text-white">
-          {name}
-        </p>
-        <p className="text-[.9rem] dark:text-white">{email}</p>
-        <div className="flex items-center gap-5">
-          <p className="text-[.9rem] capitalize dark:text-white">
-            role:{" "}
-            <span className="uppercase italic">
-              {isAuthor ? "author" : "user"}
-            </span>
-          </p>
-          <button className="text-[.7rem] underline dark:text-white">
-            {isAuthor ? "Update Author Bio" : "Become an Author"}
-          </button>
-        </div>
-      </div>
+      <UserRoleCard email={email} isAuthor={isAuthor} name={name} />
       <div className="w-10 h-10 cursor-pointer bg-lightGray dark:bg-gray-700 dark:text-white hover:bg-mediumGray transition-colors flex items-center justify-center rounded-md">
         <FaRegEdit className="text-[1.2rem]" />
       </div>
