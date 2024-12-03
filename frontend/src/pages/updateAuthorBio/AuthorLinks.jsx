04463721e51f522c602f87e1755ee35921c62c6a
Sandro <vipsandro11@gmail.com>
2024-12-03 18:17:33 +0400

    feat: create AuthorLinks.jsx

---
+++ frontend/src/pages/updateAuthorBio/AuthorLinks.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+const AuthorLinks = ({ link, handleLinkChange, index }) => {
+  return (
+    <li className="flex items-center mb-2">
+      <input
+        type="text"
+        value={link}
+        onChange={(e) => handleLinkChange(index, e.target.value)}
+        placeholder="Enter link"
+        className="flex-grow border p-2 mr-2 outline-none text-[.8rem] bg-[#F6F6F6] border-none rounded-md dark:bg-[#27272A] dark:text-white"
+      />
+    </li>
+  );
+};
+
+export default AuthorLinks;
