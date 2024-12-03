db19465eadb189779a6eb1da8657ed9b14402022
Sandro <vipsandro11@gmail.com>
2024-12-03 18:14:35 +0400

    refactor: :sparkles: Refactor generateJWT to set JWT in HttpOnly cookie
    - Modified `generateJWT` to accept `res` and set the JWT in a cookie. - Configured the cookie with HttpOnly, secure, and SameSite attributes based on environment. - The token is set to expire in 1 hour and is sent as a cookie in the response.

--- backend/utils/jwtUtils.js
+++ backend/utils/jwtUtils.js
@@ -1,19 +1,29 @@
 import jwt from "jsonwebtoken";
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const SECRET_KEY = process.env.JWT_SECRET;
 
-export const generateJWT = (user) => {
+export const generateJWT = (user, res) => {
+  const expirationTime = Date.now() + 3600000;
+
   const payload = {
     userId: user._id,
-    name: user.name,
+    name: user.name
   };
 
   const token = jwt.sign(payload, SECRET_KEY, {
-    expiresIn: "1h", 
+    expiresIn: "1h",
   });
 
-  return token;
+  // Set the cookie (JWT with HttpOnly)
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    expires: new Date(expirationTime),
+  });
+
+  return { token };
 };
