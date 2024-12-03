183dfc86bfbfe88283487a3d8644ede36edfd587
Sandro <vipsandro11@gmail.com>
2024-12-03 18:13:52 +0400

    update bookRoutes.js

--- backend/routes/bookRoutes.js
+++ backend/routes/bookRoutes.js
@@ -4,12 +4,16 @@ import {
   deleteBook,
   getAllBook,
   updateBook,
+  getSingleBook,
 } from "../controller/bookController.js";
 
 const router = Router();
 
 // get all book
 router.get("/", getAllBook);
+
+// get single book
+router.get("/:id", getSingleBook);
 
 // add new book
 router.post("/", addNewBook);
