1450cde15c5e4d168c878ecf9be96576dd75c415
Sandro <vipsandro11@gmail.com>
2024-12-03 18:12:54 +0400

    update authorController.js

--- backend/controller/authorController.js
+++ backend/controller/authorController.js
@@ -1,37 +1,35 @@
 import User from "../models/userModel.js";
 
 export const addAuthorBio = async (req, res) => {
-  const { bio, links } = req.body;
+  const { bio, link: updatedLinks } = req.body;
 
   try {
     const user = await User.findById(req.user.userId);
     if (!user) {
-      return res.status(404).send("User not found");
+      return res.status(404).json({ message: "User not found" });
     }
 
-    user.authorBio.bio = bio;
-    user.authorBio.links.push(...links);
+    if (bio) {
+      user.authorBio.bio = bio;
+    }
+    if (updatedLinks) {
+      // Validation will be handled by the schema
+      user.authorBio.links = updatedLinks;
+    }
 
     await user.save();
-    res.status(200).send({ message: "Author bio added successfully", user });
+    res
+      .status(200)
+      .json({
+        message: "Author bio added successfully",
+        authorBio: user.authorBio,
+      });
   } catch (error) {
     if (error.name === "ValidationError") {
-      const validationErrors = Object.values(error.errors).map(
-        (err) => err.message
-      );
-
-      const urlError = validationErrors.find((msg) =>
-        msg.includes("Invalid URL format")
-      );
-      if (urlError) {
-        return res
-          .status(400)
-          .send("One or more links have an invalid URL format.");
-      }
-
-      return res.status(400).send(validationErrors.join(", "));
+      return res
+        .status(400)
+        .json({ message: "One or more links have an invalid URL format." });
     }
-
-    res.status(500).send("Internal server error");
+    return res.status(500).json({ message: "Internal server error", error });
   }
 };
